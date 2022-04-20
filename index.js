fb48571cae7eb7cf6992ae8e7e732f0c04e6dbdf
David Abol Ger <draker74@gmail.com>
2022-04-20 17:05:00 +0300

    Working navigation

--- index.js
+++ index.js
@@ -1,124 +1,116 @@
 /* eslint-disable max-classes-per-file */
 class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
 }
 
 class Store {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
     }
 
-    return books;
-  }
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
 
-  static addBook(book) {
-    const books = Store.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
+    static removeBook(author) {
+        const books = Store.getBooks();
 
-  static removeBook(author) {
-    const books = Store.getBooks();
+        books.forEach((book, index) => {
+            if (book.author === author) {
+                books.splice(index, 1);
+            }
+        });
 
-    books.forEach((book, index) => {
-      if (book.author === author) {
-        books.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
-  }
+        localStorage.setItem('books', JSON.stringify(books));
+    }
 }
 
 class showBooks {
-  static displayBooks() {
-    const books = Store.getBooks();
+    static displayBooks() {
+        const books = Store.getBooks();
 
-    books.forEach((book) => showBooks.addBookToList(book));
-  }
+        books.forEach((book) => showBooks.addBookToList(book));
+    }
 
-  static addBookToList(book) {
-    const list = document.querySelector('#book-list');
-    const row = document.createElement('tr');
-    row.innerHTML = `
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td><button class="delete">Remove book</button></td>
       `;
 
-    list.appendChild(row);
-  }
-
-  static deleteBook(el) {
-    if (el.classList.contains('delete')) {
-      el.parentElement.parentElement.remove();
+        list.appendChild(row);
     }
-  }
 
-  static clearFields() {
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
-  }
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', showBooks.displayBooks);
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  const book = new Book(title, author);
-  showBooks.addBookToList(book);
-  Store.addBook(book);
-  showBooks.clearFields();
-  e.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const book = new Book(title, author);
+    showBooks.addBookToList(book);
+    Store.addBook(book);
+    showBooks.clearFields();
+    e.preventDefault();
 });
 
 document.querySelector('#book-list').addEventListener('click', (e) => {
-  showBooks.deleteBook(e.target);
+    showBooks.deleteBook(e.target);
 
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
 const bookListDiv = document.getElementById('table');
 const formDiv = document.getElementById('form');
 const contactDiv = document.getElementById('contact');
+const contactButton = document.getElementById('li-contact');
+const liForm = document.getElementById('li-list');
+const listAdd = document.getElementById('li-add');
 
-const constructListPage = () => {
-  bookListDiv.style.display = 'block';
-  formDiv.style.display = 'none';
-  contactDiv.style.display = 'none';
+function constructListPage() {
+    bookListDiv.style.display = 'block';
+    formDiv.style.display = 'none';
+    contactDiv.style.display = 'none';
 
-  renderBooks();
-};
-const constructFormPage = () => {
-  bookListDiv.style.display = 'none';
-  formDiv.style.display = 'block';
-  contactDiv.style.display = 'none';
-};
-const constructContactPage = () => {
-  bookListDiv.style.display = 'none';
-  formDiv.style.display = 'none';
-  contactDiv.style.display = 'block';
+}
+
+function constructFormPage() {
+    bookListDiv.style.display = 'none';
+    formDiv.style.display = 'block';
+    contactDiv.style.display = 'none';
+}
+
+function constructContactPage() {
+    bookListDiv.style.display = 'none';
+    formDiv.style.display = 'none';
+    contactDiv.style.display = 'block';
 };
 
-const page = (page) => {
-  switch (page) {
-    case 0:
-      constructListPage();
-      break;
-    case 1:
-      constructFormPage();
-      break;
-    case 2:
-      constructContactPage();
-      break;
-    default:
-      break;
-  }
-};
+contactButton.addEventListener('click', constructContactPage);
+listAdd.addEventListener('click', constructFormPage);
+liForm.addEventListener('click', constructListPage);
