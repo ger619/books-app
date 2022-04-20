8de8eebb596e44efe53164b7eabc465f1d156b8d
MaganaAsiati <assia.magana@gmail.com>
2022-04-20 14:33:39 +0200

    add navigation bar

--- index.js
+++ index.js
@@ -85,3 +85,40 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
 
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
+const bookListDiv = document.getElementById('table');
+const formDiv = document.getElementById('form');
+const contactDiv = document.getElementById('contact');
+
+const constructListPage = () => {
+  bookListDiv.style.display = 'block';
+  formDiv.style.display = 'none';
+  contactDiv.style.display = 'none';
+
+  renderBooks();
+};
+const constructFormPage = () => {
+  bookListDiv.style.display = 'none';
+  formDiv.style.display = 'block';
+  contactDiv.style.display = 'none';
+};
+const constructContactPage = () => {
+  bookListDiv.style.display = 'none';
+  formDiv.style.display = 'none';
+  contactDiv.style.display = 'block';
+};
+
+const page = (page) => {
+  switch (page) {
+    case 0:
+      constructListPage();
+      break;
+    case 1:
+      constructFormPage();
+      break;
+    case 2:
+      constructContactPage();
+      break;
+    default:
+      break;
+  }
+};
